f63f8480ab8498f57fee834b459d280f72635239
Nikolai Mishin <sanduku.default@gmail.com>
2024-02-14 23:31:48 +0100

    make docs script

--- internal/sdkv2provider/provider.go
+++ internal/sdkv2provider/provider.go
@@ -12,12 +12,8 @@ import (
 )
 
 func init() {
-	// Set descriptions to support markdown syntax, this will be used in document generation
-	// and the language server.
 	schema.DescriptionKind = schema.StringMarkdown
 
-	// Customize the content of descriptions when output. For example you can add defaults on
-	// to the exported descriptions if present.
 	schema.SchemaDescriptionBuilder = func(s *schema.Schema) string {
 		desc := s.Description
 		if s.Default != nil {
@@ -30,7 +26,6 @@ func init() {
 	}
 }
 
-// Provider -
 func Provider(version string, testing bool) *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
@@ -43,13 +38,11 @@ func Provider(version string, testing bool) *schema.Provider {
 		DataSourcesMap: map[string]*schema.Resource{
 			"dodo_brand": dataSourceDodoBrands(),
 		},
-		ResourcesMap: map[string]*schema.Resource{
-		},
+		ResourcesMap:         map[string]*schema.Resource{},
 		ConfigureContextFunc: providerConfigure(version, testing),
 	}
 }
 
-// APIClient Hold the API Client and any relevant configuration
 type APIClient struct {
 	client *api.APIClient
 }
